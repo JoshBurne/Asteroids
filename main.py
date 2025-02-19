64cd860eb4296ca48dd597dfbc05fa4da6230a10
Josh Burne <joshb.contact@gmail.com>
2025-02-19 09:36:03 +0000

    pygame window working with black refresh

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        #allows the user to quit using the "X"
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        #fills screen with black (0,0,0)    
+        screen.fill((0,0,0))
+        # refreshes the display
+        pygame.display.flip()
+
+
+
 
 if __name__ == "__main__":
     main()
